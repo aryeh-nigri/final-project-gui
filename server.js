af8b912ad74aef4947b7603b694ea71067ab0614
zeevNoiman <zevnoiman@gmail.com>
2019-04-21 19:55:46 +0300

    put data into table

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var fs = require('file-system');
 
 const app = express();
 
@@ -30,18 +31,27 @@ app.use('/static', express.static('public'));
 //     });
 // })
 
-app.post('/diagnosis', function (req, res) {
+app.post('/diagnosis', async function (req, res) {
     console.log("POST");
     console.log(req.body);
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var gender = req.body.gender;
-    var birthday = req.body.birthday;
-    var exam1 = req.body.exam1;
-    var exam2 = req.body.exam2;
-    var exam3 = req.body.exam3;
-    var pacsFile = req.body.pacsFile;
+    // var firstName = req.body.firstName;
+    // var lastName = req.body.lastName;
+    // var gender = req.body.gender;
+    // var birthday = req.body.birthday;
+    // var exam1 = req.body.exam1;
+    // var exam2 = req.body.exam2;
+    // var exam3 = req.body.exam3;
+    // var pacsFile = req.body.pacsFile;
+    // var pathToData = "ml_scripts/bones.csv";
+    var firstName = "joao";
+    var content = req.body.content;
 
+
+    // appendFile function with filename, content and callback function
+    await fs.appendFile('data.csv', content, function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+    });
 
     let runPy = new Promise(function (success, nosuccess) {
 
@@ -49,7 +59,7 @@ app.post('/diagnosis', function (req, res) {
             spawn
         } = require('child_process');
         //const pyprog = spawn('py', ['./ml_scripts/classifier.py', firstName, lastName, gender, birthday, exam1, exam2, exam3, pacsFile]);
-        const pyprog = spawn('py', ["./ml_scripts/test.py", firstName]);
+        const pyprog = spawn('py', ["./test.py", firstName, "data.csv"]);
 
         pyprog.stdout.on('data', function (data) {
             success(data);
@@ -66,10 +76,52 @@ app.post('/diagnosis', function (req, res) {
     });
 
     runPy.then(function (fromRunpy) {
-        console.log(fromRunpy.toString());
-        res.render('result', {
-            result: fromRunpy.toString()
-        });
+        var array = fromRunpy.toString().split("- ");
+        console.log(array);
+
+        const LR = array[0].split(": ");
+        const LDA = array[1].split(": ");
+        const KNN = array[2].split(": ");
+        const CART = array[3].split(": ");
+        const NB = array[4].split(": ");
+        const SVM = array[5].split(": ");
+
+        let indexOfChar;
+        indexOfChar = LDA[0].lastIndexOf("]");
+        const matrixConfusionLR = LDA[0].substring(0, indexOfChar + 1);
+        indexOfChar = KNN[0].lastIndexOf("]");
+        const matrixConfusionLDA = KNN[0].substring(0, indexOfChar + 1);
+        indexOfChar = CART[0].lastIndexOf("]");
+        const matrixConfusionKNN = CART[0].substring(0, indexOfChar + 1);
+        indexOfChar = NB[0].lastIndexOf("]");
+        const matrixConfusionCART = NB[0].substring(0, indexOfChar + 1);
+        indexOfChar = SVM[0].lastIndexOf("]");
+        const matrixConfusionNB = SVM[0].substring(0, indexOfChar + 1);
+        const matrixConfusionSVM = array[6];
+
+        const mlResults = {
+            LR: LR[1],
+            LDA: LDA[1],
+            KNN: KNN[1],
+            CART: CART[1],
+            NB: NB[1],
+            SVM: SVM[1],
+
+            LR_matrix: matrixConfusionLR,
+            LDA_matrix: matrixConfusionLDA,
+            KNN_matrix: matrixConfusionKNN,
+            CART_matrix: matrixConfusionCART,
+            NB_matrix: matrixConfusionNB,
+            SVM_matrix: matrixConfusionSVM,
+
+        }
+
+        console.log(mlResults);
+
+        res.json(mlResults);
+        // res.render('result', {
+        //     result: fromRunpy.toString()
+        // });
         // res.end(fromRunpy);
     });
 
@@ -124,3 +176,19 @@ app.listen(4000, () => console.log('Application listening on port 4000!'));
 //     py.stdin.end();
 
 // });
+
+
+//TODO
+/*
+- por uma foto do pacs image ao lado do choose file
+- pedir tbm id do paciente
+- adicionar uma saida para identificar o ROI e mostrar resultado na tela e esperar ishur
+- adicionar uma saida para fazer ibud tmuna a adicionar o resultado e esperar ishur  
+- adicionar uma saida para fazer as measurements e esperar ishur
+
+
+procurar PCA em python    DONE
+
+
+
+*/
--- test.py
+++ test.py
@@ -9,23 +9,42 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+from sklearn.decomposition import PCA
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import train_test_split
 
 
 firstName = sys.argv[1]     # string
+pathToData = sys.argv[2]
 
-print(firstName)
-# load dataset
-# url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
-# 6,148,72,35,0,33.6,0.627,50,1
-fileLocation = r"C:\Users\danie\OneDrive\Documentos\GitHub\final-project-gui\ml_scripts/bones.csv"
+
+fileLocation = pathToData
 # tirado da primeira linha do bones.csv :
 # Bone,meanGL,SD-GL,Smoothness,ThirdMoment,Uniformity,Entropy,CategoriaDoOsso
 # names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 names = ["Bone", "meanGL", "SD-GL", "Smoothness", "ThirdMoment", "Uniformity", "Entropy", "CategoriaDoOsso"]
 dataframe = pandas.read_csv(fileLocation, names=names)
 array = dataframe.values
-X = array[:,1:7]
-Y = array[:,7]
+
+selected = [1,2,3,4,5,6]
+
+data = array[:,selected]
+target = array[:,7]
+
+
+#pca to 2 elements and prepare graph
+pca = PCA(n_components=2, svd_solver='full')
+pca.fit(data)
+principalComponents = pca.fit_transform(data)
+principalDf = pandas.DataFrame(data = principalComponents, 
+								columns = ['principal component 1', 'principal component 2'])
+targetDf = pandas.DataFrame(data = target, columns = ['target'])
+finalDf = pandas.concat([principalDf, targetDf], axis = 1)
+finalDf.plot(kind="scatter",title="Principal Component Analisys", x="principal component 1", y="principal component 2", 
+                 alpha=0.6, c="target", cmap=plt.get_cmap("jet"), colorbar=True)
+
+
 # prepare configuration for cross validation test harness
 seed = 7
 # prepare models
@@ -36,17 +55,22 @@ models.append(('KNN', KNeighborsClassifier()))
 models.append(('CART', DecisionTreeClassifier()))
 models.append(('NB', GaussianNB()))
 models.append(('SVM', SVC()))
+
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=seed)
+
 # evaluate each model in turn
 results = []
 names = []
 scoring = 'accuracy'
 for name, model in models:
-	kfold = model_selection.KFold(n_splits=10, random_state=seed)
-	cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+	#kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, y_train, cv=3, scoring="accuracy")
+	y_train_pred = cross_val_predict(model, X_train, y_train, cv=3)
 	results.append(cv_results)
 	names.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	msg = "%s: %f %f - \n" % (name, cv_results.mean(), cv_results.std())
 	print(msg)
+	print(confusion_matrix(y_train, y_train_pred))
 # boxplot algorithm comparison
 fig = plt.figure()
 fig.suptitle('Algorithm Comparison')
